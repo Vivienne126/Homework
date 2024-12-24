9014fe697fbc8c5bc9e74b8b11e0d47a5c05a3ee
Vivienne126 <vivienne.singh@cns.ac.in>
2024-12-24 18:02:31 +0530

    new4

---
+++ Hw5.py
@@ -0,0 +1,7 @@
+n=int(input("Enter the temprature"))
+if n>=30:
+    print("You can wear light cloths")
+    print("The weather is hot")
+else:
+    print("You can wear hevy cloths")
+    print("The weather is cold")
