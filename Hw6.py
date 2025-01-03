f6f713b095e6d6696f734bdc97db01f347f71630
Vivienne126 <vivienne.singh@cns.ac.in>
2025-01-03 15:25:15 +0530

    New5

---
+++ Hw6.py
@@ -0,0 +1,13 @@
+x=(input("Enter any word"))
+
+#This is if else statement
+
+if ("a" <= x <= "z") or ("A" <= x <= "Z"):
+    print("This word is an alphabet")
+else:
+    print("This word is not a alphabet")
+
+
+
+   
+
