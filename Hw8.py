d273e288f778aa2db8040c7a069d67e6afd01dff
Vivienne126 <vivienne.singh@cns.ac.in>
2025-01-10 23:32:22 +0530

    New7

---
+++ Hw8.py
@@ -0,0 +1,17 @@
+wrong_num1=6
+wrong_num2=4
+wrong_num3=3
+wrong_num4=5
+correct_num1=3
+correct_num2=4
+correct_num3=5
+correct_num4=6
+#this is wrong number
+x=6*4+(3+5)
+print(x)
+print("this calculation is wrong")
+#This is correct calculation
+y=3*4+(5+6)
+print(y)
+print("This is right calculation")
+print("Numbershave been interchanged")
