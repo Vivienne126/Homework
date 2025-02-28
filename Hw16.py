cc156930c6e38c69b2151f79aa0f10db5d4a8df4
Vivienne126 <vivienne.singh@cns.ac.in>
2025-02-28 22:53:19 +0530

    New15

---
+++ Hw16.py
@@ -0,0 +1,13 @@
+print("Do you want to stut down your program")
+x=(input("1:Yes \n 2:No \n Enter the number"))
+if  x=="1":
+    def stutdown_program():
+        print("Stutting down the program")
+        exit()
+    stutdown_program()
+elif x=="2":
+    print("Sorry about stutdown")
+else:
+    print("sorry")
+#i guess this could be wrong Maam
+
