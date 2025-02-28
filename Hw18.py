a1e7f83015fadc972d3f60fe6494b60b1bfb2830
Vivienne126 <vivienne.singh@cns.ac.in>
2025-02-28 23:13:40 +0530

    New17

--- Hw18.py
+++ Hw18.py
@@ -1,2 +1,15 @@
-x=int(input("Enter your age"))
-try ValueError:
+def check_age():
+    while True:
+        try:
+            age=int(input("Enter your age"))
+            if age<0:
+                raise ValueError
+            break
+        except ValueError:
+            print("Input is not valid please enter a valid input")
+
+    if age % 2 == 0:
+        print("The age entered is even")
+    else:
+        print("The age entered is odd")
+check_age()
