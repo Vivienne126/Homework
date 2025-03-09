f3e62a885d4369808a3dcb46dfe54d3c72016808
Vivienne126 <vivienne.singh@cns.ac.in>
2025-03-09 13:35:22 +0530

    New20

---
+++ Hw21.py
@@ -0,0 +1,13 @@
+numbers=int(input("Enter the value u want to add in the list"))
+list.append(numbers)
+squared_numbers=[]
+def find_square_values(start,end):
+    for i in numbers:
+        squared_numbers.append(x**2)
+        print(squared_numbers)
+        print(find_square_values())
+
+#This code could be wrong can you cheack it maam.
+
+
+    
