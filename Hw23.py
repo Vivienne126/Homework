7d69c05672178b70aa7208d00a00c90ed23d98b6
Vivienne126 <vivienne.singh@cns.ac.in>
2025-04-16 19:18:48 +0530

    New22

--- Hw23.py
+++ Hw23.py
@@ -1,5 +1,9 @@
 tup_1=(4,3,2,2,-1,18)
 tup_2=(2,4,8,8,3,2,9)
 for i in tup_1:
-    product=H
+    product=i*i+1
     print("The product of the number is:" , product)
+#Product of numbers in tuple 2
+for i in tup_2:
+    product1=i*i+1
+    print("The product of numberds is {}".format(product1))
