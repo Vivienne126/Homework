f9f7640e8787dd1845e1bdca03040ca9abda9ef9
Vivienne126 <vivienne.singh@cns.ac.in>
2025-07-01 10:59:46 +0530

    New34

--- Hw35.py
+++ Hw35.py
@@ -1,12 +1,12 @@
 import pygame
 pygame.init()
 screen=pygame.display.set_mode((500,500))
-pygame.display.set_caption(("This is caption"))
-#in this line i need to upload a image but dont have a image to upload so it is showing error
+pygame.display.set_caption("This is caption")
 screen.fill((58,58,58))
+ufo_imag=pygame.image.load("ufo.png")
+screen.blit(ufo_imag,(200,200))
 done=False
 while not done:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
-    pygame.display.flip
