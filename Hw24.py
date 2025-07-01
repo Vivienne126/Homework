2d708e3c04141964cd545a5f527904370b730464
Vivienne126 <vivienne.singh@cns.ac.in>
2025-07-01 11:41:15 +0530

    New23

---
+++ Hw24.py
@@ -0,0 +1,10 @@
+set1={1,2,3}
+set2={3,4}
+print("Orignal sets:" , set1 , set2)
+set3=set1.symmetric_difference(set2)
+print("Symmetricc difference:" ,set3)
+
+set4={"blue" , "red" , "pink"}
+set5={"blue" , "pink" , "green"}
+print("Oriignal sets:" , set4 ,",",set5)
+print("Synnetric Difference:" , set4.symmetric_difference(set5))
